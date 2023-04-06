91426df5a910eb7a1e470a7742d4e58028c1965f
ashwanikumarsingh04 <singhashwani1804@gmail.com>
2023-04-06 21:59:26 +0100

    tostify edit

--- src/pages/AddEdit.js
+++ src/pages/AddEdit.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useHistory, useParams, Link } from "react-router-dom";
 import "./AddEdit.css";
 import axios from "axios";
-import { toast } from "react-toastify";
+import { ToastContainer,toast } from "react-toastify";
 
 const initialState = {
   name: "",
