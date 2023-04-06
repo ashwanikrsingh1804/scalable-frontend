4b257b897f12f2ed238f0aef8be48eaebd2ac86e
ashwanikumarsingh04 <singhashwani1804@gmail.com>
2023-04-06 21:34:45 +0100

    CRUD updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import AddEdit from "./pages/AddEdit";
+import Home from "./pages/Home";
+import View from "./pages/View";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Ashwani Kumar Singh</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer position="top-center" />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/addContact" component={AddEdit} />
+          <Route path="/update/:id" component={AddEdit} />
+          <Route path="/view/:id" component={View} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
