86b6b7074810c59854009e8e82b76963541f1083
ashwanikumarsingh04 <singhashwani1804@gmail.com>
2023-04-13 15:52:17 +0100

    edit changes

--- src/pages/AddEdit.js
+++ src/pages/AddEdit.js
@@ -33,7 +33,7 @@ const AddEdit = () => {
     } else {
       if (!id) {
         axios
-          .post("http://testenv-env.eba-e6qrxgvz.us-east-1.elasticbeanstalk.com//api/post", {
+          .post("http://testenv-env.eba-e6qrxgvz.us-east-1.elasticbeanstalk.com/api/post", {
             name,
             email,
             contact,
@@ -45,7 +45,7 @@ const AddEdit = () => {
         toast.success("Contact Added Successfully");
       } else {
         axios
-          .put(`http://testenv-env.eba-e6qrxgvz.us-east-1.elasticbeanstalk.com//api/update/${id}`, {
+          .put(`http://testenv-env.eba-e6qrxgvz.us-east-1.elasticbeanstalk.com/api/update/${id}`, {
             name,
             email,
             contact,
